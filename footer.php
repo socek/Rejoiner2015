764d133313c5774ae75c30e613d4152dad2dd5ef
Karina <kl.tovar@gmail.com>
2015-03-03 13:38:47 -0500

    Footer Responsive
    Nav in the works - sticky nag stopped working

--- footer.php
+++ footer.php
@@ -1,46 +1,50 @@
 		<nav id="footernav">
-			<section class="clearfix">
-				<div>
-					<h6>PRODUCT</h6>
-					<a href="#">Cart Abandonment</a><br>
-					<a href="#">Retention Email</a><br>
-					<a href="#">Optimization</a><br>
-					<a href="#">Automation</a><br>
-					<a href="#">Support</a><br>
-					<a href="http://docs.rejoiner.com" target="_blank">Documentation</a>
+				<div class="row">
+					<div class="large-3 medium-3 small-12 columns">
+						<h6>PRODUCT</h6>
+						<a href="#">Cart Abandonment</a><br>
+						<a href="#">Retention Email</a><br>
+						<a href="#">Optimization</a><br>
+						<a href="#">Automation</a><br>
+						<a href="#">Support</a><br>
+						<a href="http://docs.rejoiner.com" target="_blank">Documentation</a>
+					</div>
+					<div class="large-3 medium-3 small-12 columns">
+						<h6>RESOURCES</h6>
+						<a href="/cart-abandonment-rate-statistics.php">Cart Abandonment Rates</a><br>
+						<a href="/resources.php">eCommerce Growth</a><br>
+						<a href="/roi-calculator.php">ROI Calculator</a><br>
+						<a href="/cart-abandonment-templates.php">Email Templates</a><br>
+						<a href="http://docs.rejoiner.com/collection/23-faqs" target="_blank">FAQs</a><br>
+					</div>
+					<div class="large-2 medium-2 small-12 columns">
+						<h6>COMPANY</h6>
+						<a href="/about.php">About Us</a><br>
+						<a href="/contact.php">Contact Us</a><br>
+						<a href="/testimonials.php">Testimonials</a><br>
+						<a href="#">Status</a><br>
+						<a href="#">Site Map</a>
+					</div>
+					<div class="large-4 medium-4 small-12 columns">
+						<h6>BLOG</h6>
+						<script language="JavaScript" src="http://feed2js.org//feed2js.php?src=http%3A%2F%2Fblog.rejoiner.com%2Ffeed&num=2&targ=y&utf=y&html=p"  charset="UTF-8" type="text/javascript"></script>
+					</div>
 				</div>
-				<div>
-					<h6>RESOURCES</h6>
-					<a href="/cart-abandonment-rate-statistics.php">Cart Abandonment Rates</a><br>
-					<a href="/resources.php">eCommerce Growth</a><br>
-					<a href="/roi-calculator.php">ROI Calculator</a><br>
-					<a href="/cart-abandonment-templates.php">Email Templates</a><br>
-					<a href="http://docs.rejoiner.com/collection/23-faqs" target="_blank">FAQs</a><br>
-				</div>
-				<div>
-					<h6>COMPANY</h6>
-					<a href="/about.php">About Us</a><br>
-					<a href="/contact.php">Contact Us</a><br>
-					<a href="/testimonials.php">Testimonials</a><br>
-					<a href="#">Status</a><br>
-					<a href="#">Site Map</a>
-				</div>
-				<div>
-					<h6>BLOG</h6>
-					<!--
-					<a href="#">6 Zapier Recipes to Streamline Your eCommerce Business</a><br>
-					<a href="#">Should You Crowdfund Your Next Product Launch?</a>-->
-					<script language="JavaScript" src="http://feed2js.org//feed2js.php?src=http%3A%2F%2Fblog.rejoiner.com%2Ffeed&num=2&targ=y&utf=y&html=p"  charset="UTF-8" type="text/javascript"></script>
-				</div>
-			</section>
+				<!-- end row -->
 		</nav>
 		
 		<footer id="graybar">
-			<section class="clearfix">
-				<div><a href="https://twitter.com/rejoinerapp"><img src="/img/tweet.png" draggable="false"></a><a href="#">@rejoinerapp</a></div>
-				<div>(800) 284-4603&nbsp;&nbsp;|&nbsp;&nbsp;<a href="mailto:support@rejoiner.com">support@rejoiner.com</a></div>
-				<div>&copy; <?php echo date("Y") ?> Rejoiner, Inc. All Rights Reserved&nbsp;&nbsp;<span style="color:#cdcdcd">|</span>&nbsp;&nbsp;<a href="http://test.rejoiner.com/terms.php">Terms</a>&nbsp;&nbsp;<span style="color:#cdcdcd">|</span>&nbsp;&nbsp;<a href="http://test.rejoiner.com/privacy.php">Privacy</a></div>
-			</section>
+			<div class="row">
+				<div class="large-3 columns">
+					<a href="https://twitter.com/rejoinerapp"><img src="http://test.rejoiner.com/img/tweet.png" draggable="false"></a><a href="#">@rejoinerapp</a>
+				</div>
+				<div class="large-5 columns">
+					(800) 284-4603&nbsp;&nbsp;<b>|</b>&nbsp;&nbsp;<a href="mailto:support@rejoiner.com">support@rejoiner.com</a>
+				</div>
+				<div class="large-4 columns">
+					&copy; <?php echo date("Y") ?> Rejoiner, Inc. All Rights Reserved&nbsp;&nbsp;<span style="color:#cdcdcd">|</span>&nbsp;&nbsp;<a href="http://test.rejoiner.com/terms.php">Terms</a>&nbsp;&nbsp;<span style="color:#cdcdcd">|</span>&nbsp;&nbsp;<a href="http://test.rejoiner.com/privacy.php">Privacy</a>
+				</div>
+			</div>
 		</footer>
 
 <script src="http://rejoiner.com/js/plugins.js"></script>
