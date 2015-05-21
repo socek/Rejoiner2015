650341232d1604db155e3ae54dce519319e676cc
Karina <kl.tovar@gmail.com>
2015-05-21 09:38:44 -0400

    Added New Pages

--- sitemap.php
+++ sitemap.php
@@ -12,10 +12,16 @@
 	<div id="sitemap-column">
 		<h5>ABOUT REJOINER</h5>
 		<p><a href="/about">About Us</a></p>
+		<p><a href="/careers">Careers</a></p>
 		<p><a href="/contact">Contact Us</a></p>
 		<p><a href="/terms">Terms &amp; Conditions</a></p>
 		<p><a href="/privacy">Privacy</a></p>
 		<p><a href="/security">Security</a></p>
+		<br>
+		<h5>REJOINER IS FOR</h5>
+		<p><a href="/agencies">Agencies</p>
+		<p><a href="/online-retailers">Online Retailers</p>
+		<p><a href="/ticketing-vendors">Ticketing Vendors</p>
 	</div>
 	<div id="sitemap-column">
 		<h5>PRODUCT</h5>
@@ -25,8 +31,8 @@
 			<p class="listed"><a href="/#optimize">Optimize</a></p>
 			<p class="listed"><a href="/#automate">Automate</a></p>
 			<p class="listed"><a href="/#succeed">Succeed</a></p>
-		<p><a href="http://docs.rejoiner.com">Documentation</p>
 		<p><a href="/testimonials">Testimonials</p>
+		<p><a href="http://docs.rejoiner.com">Documentation</p>
 		<p><a href="/request-a-demo">Request a Demo</p>
 		<p><a href="/pricing">Pricing</p>
 		<p><a href="/cart-abandonment">Cart Abandonment</p>
@@ -38,7 +44,7 @@
 		<p><a href="/cart-abandonment-templates">Email Templates</p>
 		<p><a href="/cart-abandonment-rate-statistics">Cart Abandonment Rate Statistics</p>
 		<p><a href="/resources">Resources</p>
-			<p class="listed"><a href="/upcoming-webinars">Webinars</a></p>
+			<!--<p class="listed"><a href="/upcoming-webinars">Webinars</a></p>-->
 			<p class="listed"><a href="/resources#ebooks">eBooks</a></p>
 			<p class="listed"><a href="/resources#podcasts">Podcasts</a></p>
 			<p class="listed"><a href="/resources#presentation">Presentations</a></p>
