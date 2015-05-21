30405266fc6b906529b4e5cd2f36ae570c212069
Karina <kl.tovar@gmail.com>
2015-05-21 09:41:30 -0400

    Changed url/file name from agency to agencies

---
+++ agencies.php
@@ -0,0 +1,141 @@
+<?php
+	$pageTitle = "Rejoiner Partner Program for Agencies";
+	$metaDescription = "Agencies, partner with Rejoiner today and be seen as the hero to your clients!";
+	include("header.php");
+?>
+
+<section id="agency-page">
+	<div class="header-agency">
+    	<div class="skew-agency">
+       	 <div class="header-agency-inner">
+          <h5>Rejoiner is for agencies</h5>
+         	<h1>Rejoiner Helps Agencies Be Seen As The Ultimate Hero By Their Clients</h1>
+        	</div>
+    	</div>
+  	</div>
+
+  	<!-- here is how start -->
+  	<div id="agency-how">
+  		<div class="agency-how-text">
+  			<h5>Here is how</h5>
+  			<h1>Done-for-you or done-with-you campaign setup and management to ensure your clients succeed</h1>
+  			<span>Rejoiner's flexible approach to the needs of agencies means <b>we can act as a creative extension of your own team</b>, working behind the scenes to help you ad value to your clients, <b>or we can work directly with your client</b> to handle all of their email automation needs</span>
+  		</div>
+  		<div class="agency-how-row">
+  			<div class="one-half column">
+  				<p>
+  					If you’re looking to get a successful lifecycle email campaign up and running for your clients, but don’t want to hire a new staff member or spend hours training your existing staff on new software and best practices, you can use Rejoiner’s lifecycle email experts as an extension of your team, so that you <b>don’t tax your in house teams resources</b>.
+  				</p>
+  			</div>
+  			<div class="one-half column u-pull-right">
+  				<p>
+  					From technical integrations, to set up, to crafting beautiful highly-effective emails and optimizing campaigns to increase sales and grow revenue, <b>we can help you grow your clients businesses and been seen as the hero behind it all</b>. Think of us as your secret little weapon working along side you.
+  				</p>
+  			</div>
+  		</div>
+  	</div>
+  	<div id="quote-agency">
+  		<div id="quote-agency-image" class="lanette"></div>
+  		<div class="quote-agency-text">
+  			<p>
+  				“Several of our clients have adopted the Rejoiner solution and are having great success! As a company that is known and appreciated for its high level of client service, it was important to us to find a cart abandonment partner that meshed with the service style that our customers are accustomed to receiving. The Rejoiner team has been great. They were able to guide our clients through the entire process, from integration to creative aspects to campaign management. Highly recommended!”
+  			</p>
+  			<span><b>LANETTE WILLIS</b>, Sr. Account Manager at Nexternal Solutions</span>
+  		</div>
+  	</div>
+  	<!-- here is how end -->
+
+  	<div class="clear"></div>
+
+  	<!-- be their hero start -->
+  	<div id="agency-how">
+  		<div class="agency-how-text">
+  			<h5>Be Their Hero</h5>
+  			<h1>Help Your Clients Maximize Their Customer Lifetime Value and Reduce Their Cart Abandonment Rate By Up To 15%.</h1>
+  			<span>Agencies use Rejoiner to help their clients recover sales and reduce cart abandonment rates. Some of our customers say that their clients' cart abandonment rates decreased almost overnight.</span>
+  		</div>
+  	</div>
+  	<div id="quote-agency">
+  		<div id="quote-agency-image" class="john"></div>
+  		<div class="quote-agency-text">
+  			<p>
+  				“Rejoiner's simple yet powerful platform has helped to recover literally tens of thousands of dollars for each of our clients. Simple, easy to use, affordable and most importantly - effective. It's no wonder Rejoiner has become one of the core services we recommend to each of our clients.”
+  			</p>
+  			<span><b>John Richardson</b>, President, Swank Internet Business Solutions LLC</span>
+  		</div>
+  	</div>
+  	<!-- be their hero end -->
+
+	<div class="clearfix"></div>
+
+  	<!-- benefits start -->
+  	<div id="agency-how">
+  		<div class="agency-how-text">
+  			<h5>TOP FEATURES</h5>
+  			<h1>Top fatures and benefits for agencies and their clients</h1>
+  		</div>
+  		<div class="agency-how-row">
+  			<div class="one-half column">
+  				<h3>
+  					Run New A/B Tests For Your Clients Each Month To Help Them Increase Conversion Rates and Grow Revenue
+  				</h3>
+  				<p>
+  					If you’re looking to get a successful lifecycle email campaign up and running for your clients, but don’t want to hire a new staff member or spend hours training your existing staff on new software and best practices, you can use Rejoiner’s lifecycle email experts as an extension of your team, so that you <b>don’t tax your in house teams resources</b>.
+  				</p>
+  			</div>
+  			<div class="one-half column">
+  				<img src="img/industries/agency/ab-test-agency.png">
+  			</div>
+  		</div>
+
+  		<div class="agency-how-row" style="margin-top:8%;">
+  			<div class="one-half column">
+  				<img src="img/industries/agency/multiuser-agency.png">
+  			</div>
+  			<div class="one-half column">
+  				<h3>
+  					Manage Multiple Client Accounts From One Dashboard
+  				</h3>
+  				<p>
+  					If you’re managing an agency, it’s likely that you have multiple clients and internal team members using all kinds of software. Rejoiner helps you keep things organized and running smoothly by allowing you to manage multiple client accounts — all from one dashboard. You can even provide a login for each client so they can log into their own sub account for their business and they won’t be able to view any of your other client accounts.
+  				</p>
+  			</div>
+  		</div>
+
+  		<div class="agency-how-row" style="margin-top:8%;">
+  			<div class="one-half column">
+  				<h3>
+  					Easily Share Reports With Your Clients
+  				</h3>
+  				<p>
+  					Rejoiner lets you to share a dashboard of important numbers with your clients and show them how much additional revenue you are bringing to their business.
+  				</p>
+  			</div>
+  			<div class="one-half column">
+  				<img src="img/industries/agency/reports-agencies.png">
+  			</div>
+  		</div>
+
+  	</div>
+  	<!-- here is how end -->
+
+      <div class="clearfix" style="margin-bottom:7%;"></div>
+
+    <article id="footer-cut">  
+      <div class="footer-box">
+          <div class="footer-inner">
+            <div class="footer-content">
+              <h5>Rejoiner is for agencies</h5>
+              <h1>Earn Revenue Share For Referred Customers</h1>
+              <a id="cta-slant" href="/request-a-demo" class="agency" onClick="ga('send', 'event', { eventCategory: 'mainsite', eventAction: 'demorequestclick', eventLabel: 'industryAgency'});">BECOME A PARTNER & A CLIENT HERO</a>
+            </div>
+          </div>
+      </div>
+  </article>
+
+
+</section>		
+
+
+			
+<?php include("footer.php"); ?>
