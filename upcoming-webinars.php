00fe29a15cf86eb6885e47bcf33004a103ddcc22
Karina Tovar <kl.tovar@gmail.com>
2015-05-22 16:13:04 -0400

    HTML Linting part 6

--- upcoming-webinars.php
+++ upcoming-webinars.php
@@ -6,48 +6,39 @@
 		
 <section id="webinar">
 	<h1>LEARN TO AUTOMATE ECOMMERCE GROWTH</h1>
-<p class="white">Join us every other month for a new presentation on automated eCommerce growth strategies.</p>
+	<p class="white">Join us every other month for a new presentation on automated eCommerce growth strategies.</p>
 </section>
 
 <section id="next-webinar" class="clearfix">
 	<section>
-	<div>
-		<h5>OUR NEXT WEBINAR</h5>
-		<h4>Introduction to eCommerce Lifecycle Marketing</h4>
-		<p>Learn how to use email to engage your customers throughout the entire customer lifecycle. We'll cover strategies to help you turn first-time visitors into subscribers and new customers into loyal brand advocates — on auto-pilot.</p>
-		
-	</div>
-	<div>
-		<p><img style="margin-right:10px;" src="img/webinars/clock-icon.png" draggable="false"><b>April 30th</b>, 2015 at <b>1:00pm</b> EST</p>
-<a class="register" href="https://attendee.gotowebinar.com/register/6260012643636470530">Register Now</a>
-	</div>
+		<div>
+			<h5>OUR NEXT WEBINAR</h5>
+			<h4>Introduction to eCommerce Lifecycle Marketing</h4>
+			<p>Learn how to use email to engage your customers throughout the entire customer lifecycle. We'll cover strategies to help you turn first-time visitors into subscribers and new customers into loyal brand advocates — on auto-pilot.</p>
+		</div>
+		<div>
+			<p><img style="margin-right:10px;" src="img/webinars/clock-icon.png" draggable="false"><b>April 30th</b>, 2015 at <b>1:00pm</b> EST</p>
+			<a class="register" href="https://attendee.gotowebinar.com/register/6260012643636470530">Register Now</a>
+		</div>
 	</section>
 </section>
 
 <section id="webinar-quote">
 	<p class="quote">"Rejoiner is easy to set up, simple to use, gives you some amazing insights into customer shopping habits."</p>
-	
 	<p>Casie Gillette, Online Marketing Manager at Grasshopper</p>
-	
 	<img src="img/down-arrow-line.png">
 </section>
 
 <section id="upcoming" class="clearfix">
-	
-	<h3 style="margin-bottom: 50px;">Our Upcoming Free Webinars</h3>
-	
+	<h3 style="margin-bottom: 50px;">Our Upcoming Free Webinars</h3>	
 	<div class="event clearfix">
 		<h5>May 29th, 2015 at 1:00pm EST</h5>
-		
 		<p>Introduction to eCommerce Lifecycle Marketing</p>
-		
 		<a class="register" href="https://attendee.gotowebinar.com/register/1635754809257121538" target="_blank">Register Now</a>
 	</div>
 	<div class="event clearfix">
 		<h5>June 30th, 2014 at 1:00pm EST</h5>
-		
 		<p>Introduction to eCommerce Lifecycle Marketing</p>
-		
 		<a class="register" href="https://attendee.gotowebinar.com/register/6467812644704779010" target="_blank">Register Now</a>
 	</div>
 </section>
