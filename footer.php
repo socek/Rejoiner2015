506967b2a9d085aea997c5a20443f743e79e04a7
Karina Tovar <kl.tovar@gmail.com>
2015-09-24 10:19:42 -0400

    Added Tracking Script to all pages - footers

--- footer.php
+++ footer.php
@@ -74,6 +74,23 @@
 <script src="/js/forms.js"></script>
 
 
+<script type="text/javascript">
+    var trackcmp_email = '';
+    var trackcmp = document.createElement("script");
+    trackcmp.async = true;
+    trackcmp.type = 'text/javascript';
+    trackcmp.src = '//trackcmp.net/visit?actid=251595229&e='+encodeURIComponent(trackcmp_email)+'&r='+encodeURIComponent(document.referrer)+'&u='+encodeURIComponent(window.location.href);
+    var trackcmp_s = document.getElementsByTagName("script");
+    if (trackcmp_s.length) {
+        trackcmp_s[0].parentNode.appendChild(trackcmp);
+    } else {
+        var trackcmp_h = document.getElementsByTagName("head");
+        trackcmp_h.length && trackcmp_h[0].appendChild(trackcmp);
+    }
+</script>
+
+
+
         <script>
         window.intercomSettings = {
             app_id: "5qjle8l9",
