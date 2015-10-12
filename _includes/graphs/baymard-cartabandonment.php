ef5b97a32d23ed88bc3f475caf0e63a332eae6c5
Karina <kl.tovar@gmail.com>
2015-10-12 19:24:13 -0400

    Added Data Driven Guide page, started creating styles for that page and added a graph section to the includes with the Baymard graph

---
+++ _includes/graphs/baymard-cartabandonment.php
@@ -0,0 +1,39 @@
+<style id="jsbin-css">
+.morris-hover {display: block; margin: 2% auto; background-color: #EFEFEF;padding: 5px 0px 0px;max-width: 290px;}
+.morris-hover-row-label {display:block;padding: 10px; max-width: 30px;font-family: 'museo-sans', Helvetica, Arial, sans-serif; font-size: 13px; text-align: right; float: left;}
+.morris-hover-point{display:block;padding: 10px 0px; color: #999 !important; font-family: 'museo-sans', Helvetica, Arial, sans-serif;font-size: 13px; text-align: left;float: left; max-width: 250px;}
+#line-example {width: 100%; margin: 4% auto 2%; overflow: scroll;}
+path {stroke: rgb(96, 175, 56);}
+circle {fill: rgb(96, 175, 56);}
+</style>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+<script src="http://cdn.oesmith.co.uk/morris-0.4.1.min.js"></script>
+<script src="//use.typekit.net/bon1cvx.js"></script>
+
+
+<div id="line-example"></div>
+
+<script id="baymard-graph">
+Morris.Line({
+  element: 'line-example',
+  data: [
+    { y: '2006', a: 59.8 },
+    { y: '2007', a: 61.36 },
+    { y: '2008', a: 62.01 },
+    { y: '2009', a: 52.92 },
+    { y: '2010', a: 51.812 },
+    { y: '2011', a: 51.655 },
+    { y: '2012', a: 59.985 },
+    { y: '2013', a: 62.27 },
+    { y: '2014', a: 50.67 },
+    { y: '2015', a: 75.3 }
+  ],
+  xkey: 'y',
+  ykeys: ['a'],
+  labels: ['Cart Abandonment Average'],
+  units: '%',
+  resize: true
+});
+</script>
---
+++ data-driven-guide.php
@@ -0,0 +1,374 @@
+<?php
+	$pageTitle = "The Data-Driven Guide to Abandoned Cart Email &amp; Cart Abandonment Remarketing";
+	$metaDescription = "ADD DESCRIPTION HERE";
+    include("special-header.php");
+	include("_includes/funnel-forms/liftopia-case-popup.php");
+    $socialLinks = "dataGuide";
+    $fbLink = "#";
+    $twLink = "#";
+?>
+
+
+<!-- special page header -->
+<div id="special-header">
+	<div class="top-bar">
+		<div class="left-side">
+			<a class="white-logo" href="/"></a>
+		</div>
+		<div class="right-side">
+			<a class="down-book liftopiaCS-trigger" href="#">Download Guide</a>
+			<div class="share" style="border-color: <?php echo $highColor; ?>;">
+				<span>SHARE</span>
+					<a href="<?php echo $fbLink; ?>" title="Share on Facebook" class="facebook facebook-share share-btn" data-js="facebook-share">&nbsp;&nbsp;</a>
+					<a href="<?php echo $twLink; ?>" title="Share on Twitter" class="twitter twitter-share share-btn" data-js="twitter-share">&nbsp;&nbsp;</a>
+					<a href="#" title="#" class="linkedin">&nbsp;&nbsp;&nbsp;</a>
+			</div>
+		</div>
+	</div><!-- end top bar -->
+</div><!-- end special-header div -->
+
+<section id="special-page-section">
+
+    <div class="banner dataGuide-bg">
+        <div class="headline">
+			<div id="ribbon-header">
+				<span class="left-ribbon green-left"></span>
+				<span class="ribbon ribbon-green">THE DATA-DRIVEN GUIDE TO</span>
+				<span class="right-ribbon green-right"></span>
+			</div>
+            <h1>Abandoned Cart Email &amp; Cart Abandonment Remarketing</h1>
+        </div>
+        <div class="triangle-up">
+			<img src="/img/special-pages/cartguide/main-hd-triangle.png" />
+		</div>
+    </div>
+
+    <div id="intro" class="intro cartguide">
+        <div class="left-align-txt">
+			<h5 class="dark-purple">Introduction</h5>
+	        <span class="guide-pulltxt">The idea that 70% online consumers engage with your site to the point where they are about to <a href="#" target="_blank">check out and abandon</a> is nerve wracking.</span>
+			<p>The Baymard Institute, an eCommerce usability think tank, has aggregated cart abandonment data from various industry sources over the last 9 years. <strong class="medium-bold">Bad news:</strong>  Despite major advancements in technology and eCommerce user experience design, the average cart abandonment rate has remained constant.</p>
+		</div>
+		<div style="margin-top:30px;border: 3px solid #efefef;background: #fbfafa;">
+			<?php include("_includes/graphs/baymard-cartabandonment.php"); ?>
+		</div>
+		<span class="caption" style="margin-bottom:4%;">
+	            Source: <a href="#" target="_blank">Baymard Institute</a>
+	    </span>
+		<div class="left-align-txt">
+			<p>Industry pundits portray cart abandonment as a problem that costs online retailers billions of dollars in lost revenue every year. <strong class="medium-bold">Good news:</strong> This is an exaggeration. It’s human nature to test the waters before making a purchase. There’s no technology in the world that will eliminate it. We must accept the fact that cart abandonment is a very natural part of the eCommerce buying cycle.</p>
+			<span class="guide-pulltxt breather">What we can do is change our perspective and see shopping cart abandonment as a conversion rate optimization and customer service opportunity for us as eCommerce professionals.</span>
+			<p>How? By taking a customer-centric approach. Customers are giving you an amazing signal of purchase intent when they abandon a transaction on your site. Think of cart abandonment emails as your second chance to continue the conversation with this incredibly important group of potential customers.</p>
+			<p><strong class="medium-bold">By building a quality abandoned cart email program, one of three things will happen when you follow up:</strong></p>
+			<div style="border-left: 4px solid #bbd7ac; height:250px; width: 90%; margin: 40px auto;padding: 10px 30px 10px;">list pointers placeholder</div>
+			<p>This guide is written to help you maximize those three outcomes with every cart abandonment email that you send. In addition, you’ll learn to measure the cart abandonment rate on your site, how to identify a higher
+percentage of customers shopping on your site and how to calculate the expected ROI of an abandoned cart email solution.</p>
+		</div>
+    </div>
+	<div class="clearfix"></div>
+
+    <div class="pasty-nav">
+        <div class="content">
+			<div id="pasty-nav-anchor"></div>
+			<nav>
+				<ul class="navHighlighter">
+					<li class="logo-r"><a href="/"><i></i></a></li>
+		            <li><a href="#opportunity">The Opportunity</a></li>
+		            <li><a href="#integration">Integration</a></li>
+		            <li><a href="#campaign">The Campaign</a></li>
+					<li><a href="#templates-creative">The Creative</a></li>
+		            <li><a href="#results">The Results</a></li>
+		            <li class="download-guide-cta liftopiaCS-trigger"><span>Download PDF Case Study</span></li>
+				</ul>
+			</nav>
+        </div>
+    </div>
+
+    <div id="opportunity" class="text-content scrollto">
+		<div>
+	        <h5>THE OPPORTUNITY</h5>
+	        <h2>Why Liftopia Was Missing Out On $1.3 Million In Revenue</h2>
+	        <p>Imagine a customer moving from step to step on an eCommerce website. They browse product pages, add to cart, and start the process of filling out the checkout form. The customer takes out their credit card, fills out a few checkout form fields and for one of a hundred diferent reasons, decides to abandon the transaction. The card goes back in the wallet and the sale is lost. This situation occurs millions, probably billions, of times per day on eCommerce websites.</p>
+	        <span class="quote blue">
+	            In just the first month of using Rejoiner, Liftopia discovered they were losing almost $1.3 million dollars in revenue from this exact situation.
+	        </span>
+	        <img src="/img/special-pages/liftopia/dashboard-liftopia-01.jpg" draggable="false" alt="Liftopia Dashboard"/>
+	        <span class="caption">
+	            SCREENSHOT OF REJOINER ACCOUNT FROM 12/1/2013 - 12/31/2013
+	        </span>
+	        <p>From the moment that customers began the checkout process on Liftopia.com, they were able to ascertain that 51% were abandoning carts prior to completion. Rejoiner’s ‘pre-submit tracking’ feature was able to identify 24% of those abandoners (even though these users didn’t click ‘submit’), yielding almost another 8,000 potential customers for Liftopia to remarket to and recover more revenue.</p>
+		</div>
+    </div>
+
+    <div id="integration" class="text-content skybg scrollto">
+		<div>
+	        <h5>INTEGRATION</h5>
+	        <h2>Using Rejoiner's Google Tag Manager Integration, Liftopia Was Integrated In Minutes, Not Days or Weeks</h2>
+	        <p>Liftopia deployed Rejoiner to Liftopia.com using Google Tag manager, sending transactional data from the cart for personalization, segmentation and reporting purposes. There were some initial challenges to overcome with deploying via GTM, but we here at Rejoiner, provided hands-on technical support to overcome them. The integration was deployed, tested and activated within a matter of days due to this tight collaboration.</p>
+	        <span class="quote blue">
+	            Better yet, Liftopia recovered their first cart within hours of activating the campaign.
+	        </span>
+		</div>
+    </div>
+
+    <div id="campaign" class="text-content scrollto">
+		<div>
+	        <h5>THE CAMPAIGN</h5>
+	        <h2>The Strategies Liftopia Used To Recover Lost Revenue On Auto-Pilot</h2>
+	        <p>
+	            In similar collaborative fashion, Liftopia worked hand in hand with the Rejoiner creative team to design and develop a two email cart abandonment email sequence.
+	            <br>
+	            <strong class="list-intro light-blue">
+	                Here’s a list of the key components the Liftopia campaign included:
+	            </strong>
+	        </p>
+
+	        <strong class="highlight cyanbg">An Automated Cart Abandonment Email Sequence</strong>
+	        <p>
+	            Made up of three emails that fired 30 minutes and 24 hours after the visitor abandoned cart. <strong>Customers who converted in the midst of the campaign were removed from the campaign automatically and emails were delivered on auto-pilot.</strong> Once the campaign launched, the amount of resources required to manage the campaign was basically zero. <strong>It’s like having an additional salesperson working for the team 24/7, 365 days a year.</strong>
+        </p>
+
+	        <strong class="highlight cyanbg">Intelligent Frequency Capping For Returning And Existing Customers</strong>
+	        <p>
+	            We placed certain limits on the number of times a customer could receive the campaign. Frequency capping ensured that the campaign was always “in step” with customers and would never annoy anyone or ruin your brand image. It also caught customers who abandon on purpose in search of a discount.
+	        </p>
+
+	        <strong class="highlight cyanbg midpointTrigger">Dynamic Personalization Of The Creative</strong>
+	        <p>
+	            We added the specific tickets/dates that the customer had left in their cart, as well as a personalized salutation. <strong>Personalizing the creative with the actual tickets left behind</strong> created context by reminding the customer and <strong>drove higher click-through rates &amp; conversion from the emails.</strong> This provided a big revenue boost.
+        	</p>
+
+	        <strong class="highlight cyanbg">Friendly Customer Service Tone</strong>
+	        <p>
+	            Using a customer service tone makes customers feel comfortable enough to reply to the email with questions or to call Liftopia at their toll-free number for support and finish the sale. It also generated an enormous amount of <strong>qualitative feedback</strong> about where the friction points existed in customer’s minds, which was proactively addressed on checkout, which helped Liftopia further improve their checkout funnel and reduce their cart abandonment rate even more.
+	        </p>
+
+	        <strong class="highlight cyanbg">Dynamic Cart Regeneration</strong>
+	        <p>
+	            Dynamic cart regeneration that recreated the user’s previous session across mobile, desktop and tablet devices when they clicked through the email. The result was that no matter where a customer opened a Rejoiner email, they were able to effortlessly return to the site and complete the checkout process fast and without any additional hassles or friction points stopping them from completing sale. <strong>Less friction = higher conversions.</strong>
+	        </p>
+
+	        <strong class="highlight cyanbg">Bullet-Proof Responsive Templates</strong>
+	        <p>
+	            Responsive HTML templates that rendered perfectly no matter where the customer opens the emails. Responsive email templates created the <strong>best possible experience for customers</strong>, which in turn drove more clicks, new customers and revenue growth for Liftopia.
+	        </p>
+		</div>
+    </div>
+
+    <div id="templates-creative" class="text-content skybg scrollto">
+		<div>
+	        <h5>THE CREATIVE</h5>
+			<div class="creative-01 liftopia-temp01"></div>
+	        <div class="creative-02 liftopia-temp02"></div>
+		<div id="phones-creative">
+				<div id="phoneleft">
+					<div class="phone-container">
+						<div class="iphone">
+							<div class="btnCima"></div>
+							<div class="head">
+								<div class="camera"></div>
+								<div class="buraco"></div>
+								<div class="traco"></div>
+							</div>
+							<div class="screen">
+								<img src="/img/special-pages/liftopia/01-liftopia-mobile.jpg" />
+							</div>
+							<div class="down">
+								<div class="phonebtn">
+									<div class="phonebtn2"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div id="phoneright">
+					<div class="phone-container">
+
+						<div class="iphone">
+							<div class="btnCima"></div>
+							<div class="head">
+								<div class="camera"></div>
+								<div class="buraco"></div>
+								<div class="traco"></div>
+							</div>
+							<div class="screen">
+								<img src="/img/special-pages/liftopia/02-liftopia-mobile.jpg" />
+							</div>
+							<div class="down">
+								<div class="phonebtn">
+									<div class="phonebtn2"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+
+    <div id="results" class="text-content scrollto">
+		<div>
+	        <h5>THE RESULTS</h5>
+	        <h2 class="larger">$714,000+ Recovered Over 4 Month Season</h2>
+	        <p>Liftopia added $714,251.16 to their top-line revenue from 11/1/2013 - 3/1/2013 as a result of the campaign. For every email Rejoiner sent, Liftopia made $9.23 and remarketed to over 27,000 individual customers during the Winter 2013 season.</p>
+	        <span class="quote blue">
+	            Without Rejoiner, 17% of the company's total revenue would have been lost due to cart abandonment.
+	        </span>
+	        <img src="/img/special-pages/liftopia/dashboard-liftopia-02.jpg" draggable="false"/ alt="Liftopia Campaign Results" />
+	        <span class="caption">
+	            SCREENSHOT OF LIFTOPIA'S CAMPAIGN RESULTS FROM 11/1/2013 - 3/1/2013
+	        </span>
+	        <div class="end-quote">
+	            <i>
+	                "Rejoiner gave us a big boost in revenue, we saw immediate results with customers completing their abandoned carts at a 50% higher rate than our previous provider. We recovered just over $714,000.00 and are so happy with how Rejoiner helped us through every step of the process so that we were able to have a seamless integration and start recovering revenue right away."
+	            </i>
+	            <p><strong>Jason Zhang</strong>, Product Manager, Liftopia</p>
+	        </div>
+		</div>
+    </div>
+
+
+    <div class="last-call darkpurple">
+		<div>
+			<div class="lft-frm">
+		        <h3>Thanks for reading!</h3>
+				<p>Learned a lot? Sharing is caring. Send the eBook to your friends.</p>
+				<br />
+				<div class="share-end-call">
+					<ul>
+						<li><a href="<?php echo $twLink; ?>" class="twBlue icon-twitter share-btn tweet-rectangle">Tweet This!</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="right-image">
+                <img src="/img/special-pages/liftopia/lft-ebook-image.png" alt="Liftopia Case Study eBook" draggable="false" />
+            </div>
+		</div>
+    </div>
+
+
+</section>
+
+<!-- stickyNavbar plugin -->
+<script src="//cdn.jsdelivr.net/stickynavbar.js/1.3.0/jquery.stickyNavbar.min.js"></script>
+
+
+<script>
+var mq = window.matchMedia('all and (min-width: 850px)');
+// Create a clone of the menu, right next to original.
+$('.pasty-nav').addClass('original').clone().insertAfter('.pasty-nav').addClass('cloned fixed').removeClass('original').hide();
+scrollIntervalID = setInterval(stickIt, 10);
+
+function stickIt() {
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;
+
+  if ($(window).scrollTop() >= (orgElementTop) && mq.matches) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('top',0).css('width',widthOrgElement).show();
+    $('.original').css('visibility','hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
+  }
+};
+
+//* Pasty-Nav Anchor Links Get Activated
+//*  through stickyNavbar plugin
+
+$(function () {
+ $('.pasty-nav').stickyNavbar();
+});
+
+//* custom smooth scroll for special pages
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	  var target = $(this.hash);
+	  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	  if (target.length) {
+		$('html,body').animate({
+		  scrollTop: target.offset().top -20
+		}, 300);
+		return false;
+	  }
+	}
+  });
+});
+</script>
+
+
+<!-- Popup appears on scroll after hitting ".midpointTrigger" -->
+<script type="text/javascript">
+// * https://github.com/shorttompkins/scrollToggle/blob/master/README.md
+var ScrollToggle = function (top, callbackShow, callbackHide) {
+    this.ontop = 0;
+    this.hontop = 0;
+    this.top = top + 20;
+    this.show = callbackShow;
+    this.hide = callbackHide;
+    var self = this;
+    (function () {
+        $(window).scroll(function (event) {
+            var y = $(window).scrollTop();
+            if (y >= self.top)
+                self.ontop = 1;
+            else
+                self.ontop = 0;
+            if (self.ontop !== self.hontop) {
+                if (self.ontop) {
+                    self.show();
+                }
+                else {
+                    self.hide();
+                }
+            }
+            self.hontop = (self.ontop * 1);
+        });
+    })();
+};
+//ScrollToggle(verticalScrollTriggerPosition, callbackScrolledTO, callbackScrolledAWAY)
+//verticalScrollTriggerPosition can be a number, property, or computed. It is the vertical Y coord of the scrollbar destination.
+
+var getCookie = function(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+};
+
+var myScroller = new ScrollToggle($('.midpointTrigger').position().top, function () {
+	if(getCookie('popupCaseSeen') !== 'true') {
+	    console.log("Element has been reached - Show modal.");
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+		$('body').addClass('stop-scrolling');
+	}
+}, function () {
+	if(getCookie('popupCaseSeen') !== 'true') {
+	    console.log("Element is gone - Hide Modal");
+	    //set cookie
+	    var d = new Date();
+	    d.setTime(d.getTime() + (24*60*60*1000));
+	    var expires = "expires="+d.toUTCString();
+	    document.cookie = "popupCaseSeen=true" + "; " + expires;
+	}
+});
+</script>
+
+
+
+<?php include("special-footer.php"); ?>
